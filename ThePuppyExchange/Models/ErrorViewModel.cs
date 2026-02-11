6f20c3fbaa30746cfcb7d7fe4026de9a7fd14022
mselvi <mselvi@TC-14>
2026-02-11 12:23:03 -0600

    Add project files.

---
+++ ThePuppyExchange/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ThePuppyExchange.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
