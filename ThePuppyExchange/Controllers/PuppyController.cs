6fd263462c4782c7cfc354033d2ed7cd8d931ce7
Jacob-N-Sanchez <jsanchez17@my.dom.edu>
2026-03-30 11:00:59 -0500

    Updated catalog.cshtml and puppycontroller to add search filters for the sex and breed characteristics

--- ThePuppyExchange/Controllers/PuppyController.cs
+++ ThePuppyExchange/Controllers/PuppyController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLogicLayer;
 using DataAccessLayer.Data;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,18 +16,37 @@ namespace ThePuppyExchange.Controllers
             this.puppyDbContext = puppyDbContext;
         }
 
-        public async Task<IActionResult> Catalog()
+        public async Task<IActionResult> Catalog(string selectedBreed, string selectedSex)
         {
+            var query = puppyDbContext.Puppy.AsQueryable();
+
+            if (!string.IsNullOrEmpty(selectedBreed))
+            {
+                query = query.Where(p => p.breed == selectedBreed);
+            }
+
+            if (!string.IsNullOrEmpty(selectedSex))
+            {
+                query = query.Where(p => p.sex.ToLower() == selectedSex.ToLower());
+            }
+
+            var puppies = await query.ToListAsync();
+
+            var allBreeds = await puppyDbContext.Puppy.Select(p => p.breed).Distinct().ToListAsync();
+
+            var allSexes = new List<string> { "M", "F" };
+
+            ViewData["Breeds"] = allBreeds;
+            ViewData["Sexes"] = allSexes;
+            ViewData["SelectedBreed"] = selectedBreed;
+            ViewData["SelectedSex"] = selectedSex;
             ViewData["ShowLogout"] = true;
-            var puppies = await puppyDbContext.Puppy.ToListAsync();
 
             return View(puppies);
         }
 
         public IActionResult ProductPage(int id)
         {
-            ViewData["ShowLogout"] = true;
-
             var puppy = puppyDbContext.Puppy
                 .FirstOrDefault(p => p.product_id == id);
 
